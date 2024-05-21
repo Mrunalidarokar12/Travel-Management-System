6fd8b95a9dee161f4866a538292fa71c6bf4bb28
ASUS <ASUS@LAPTOP-M935OFDG>
2024-05-21 23:09:20 +0530

    Travel Management System

---
+++ src/mysql.sql
@@ -0,0 +1,11 @@
+create database travelmanagementsystem;
+
+show databases;
+
+use travelmanagementsystem;
+
+create table account (username varchar(20), name varchar(20), password varchar(20), security varchar(100), answer varchar(50));
+
+show tables;
+
+select*from account;
